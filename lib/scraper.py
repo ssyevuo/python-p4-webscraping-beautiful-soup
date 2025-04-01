fa2a43579bb58a3a3ab304b7dc1ed64a5d6b2f5c
Shakira Syevuo <shakirasyevuo@gmail.com>
2025-04-01 14:26:36 +0300

    Completed code-along

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,6 +1,11 @@
 from turtle import ht
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup # for beautiful soup
 import requests
 
+# to prevent a 403 error
 headers = {'user-agent': 'my-app/0.0.1'}
 html = requests.get("https://flatironschool.com/", headers=headers)
+
+doc = BeautifulSoup(html.text, 'html.parser')
+
+print(doc.select('.heading-financier'))
